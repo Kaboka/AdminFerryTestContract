9acb6ddd4073aa7dfd102d7e7e9c7ae0ec3e47b3
Kasper <Kasper@Kasper-laptop>
2014-12-20 20:12:48 +0100

    final test

--- src/dk/cphbusiness/schedule/AddScheduleTest.java
+++ src/dk/cphbusiness/schedule/AddScheduleTest.java
@@ -2,12 +2,17 @@ package dk.cphbusiness.schedule;
 
 import dk.cphbusiness.test.FerryManagerHolder;
 import ferry.contract.AdminContract;
+import ferry.dto.DepartureDetail;
+import ferry.dto.FerryConfigDetail;
+import ferry.dto.RouteDetail;
 import ferry.dto.ScheduleDetail;
-import ferry.dto.ScheduleIdentifier;
-import java.util.Collection;
+import ferry.eto.InvalidDateException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import org.junit.After;
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -32,21 +37,55 @@ public class AddScheduleTest {
     public void tearDown() {
     }
 
- /*   @Test
+    @Test
     public void testAddScheduleSuccessfully() throws Exception {
-        ScheduleDetail schedule = new ScheduleDetail(1, new Date(), new Date(), null);
+        
+        ArrayList<RouteDetail> routes = (ArrayList<RouteDetail>) manager.showRoutes();
+        assertTrue("Not empty", !routes.isEmpty());
+        ArrayList<FerryConfigDetail> configs = (ArrayList<FerryConfigDetail>) manager.showFerryConfigs();
+        assertTrue("Not empty", !configs.isEmpty());
+        FerryConfigDetail chosenConfig = configs.get(0); 
+        ArrayList<DepartureDetail> depatures = new ArrayList<>();
+        DateFormat dateFormat = new SimpleDateFormat("DD-MM-YYYY");
+        
+        depatures.add(new DepartureDetail(10, dateFormat.parse("21-12-3500"), 1,routes.get(0),chosenConfig));   
+        ScheduleDetail schedule = new ScheduleDetail(1, dateFormat.parse("21-12-9000"), dateFormat.parse("21-12-3000"), depatures);
         manager.addSchedule(schedule);
-        Collection<ScheduleDetail> schedules = manager.showSchedules();
-        ScheduleDetail tempSchedule = null;
-        for (ScheduleDetail s : schedules) {
-            if (s.getID() == 1) {
-                tempSchedule = s;
-            }
-        }
-        Assert.assertEquals(schedule.getID(), tempSchedule.getID());
     }
     
-    public void testAddScheduleFailure() {
+    @Test(expected = InvalidDateException.class)
+    public void testAddingScheduleInvalidDate() throws Exception {
+        ArrayList<RouteDetail> routes = (ArrayList<RouteDetail>) manager.showRoutes();
+        assertTrue("Not empty", !routes.isEmpty());
+        ArrayList<FerryConfigDetail> configs = (ArrayList<FerryConfigDetail>) manager.showFerryConfigs();
+        assertTrue("Not empty", !configs.isEmpty());
+        FerryConfigDetail chosenConfig = configs.get(0); 
+        ArrayList<DepartureDetail> depatures = new ArrayList<>();
+        depatures.add(new DepartureDetail(10, new Date(), 1,routes.get(0),chosenConfig));   
+ 
+        DateFormat dateFormat = new SimpleDateFormat("DD-MM-YYYY");
+        Date startDate  = dateFormat.parse("05-01-1864");
+        Date endDate = dateFormat.parse("22-01-2015");
+        ScheduleDetail schedule = new ScheduleDetail(1, endDate,startDate, depatures);
+        manager.addSchedule(schedule);
+    }
     
-    }*/
+   @Test(expected = InvalidDateException.class)
+    public void testAddingDepaturedateNotInScheduleDate() throws Exception {
+        ArrayList<RouteDetail> routes = (ArrayList<RouteDetail>) manager.showRoutes();
+        assertTrue("Not empty", !routes.isEmpty());
+        ArrayList<FerryConfigDetail> configs = (ArrayList<FerryConfigDetail>) manager.showFerryConfigs();
+        assertTrue("Not empty", !configs.isEmpty());
+        FerryConfigDetail chosenConfig = configs.get(0); 
+                
+        DateFormat dateFormat = new SimpleDateFormat("DD-MM-YYYY");
+        ArrayList<DepartureDetail> depatures = new ArrayList<>();
+        depatures.add(new DepartureDetail(10, dateFormat.parse("10-12-2014"), 1,routes.get(0),chosenConfig));           
+        
+        Date startDate  = new Date();
+        Date endDate = dateFormat.parse("22-01-9000");
+        
+        ScheduleDetail schedule = new ScheduleDetail(1,endDate,startDate, depatures);
+        manager.addSchedule(schedule);
+    }
 }
